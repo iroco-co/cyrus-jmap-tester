06f2a04eea7fe68172474d7abac3e5894b675225
Bruno Thomas <bruno@barreverte.fr>
2024-02-02 19:16:30 +0100

    feat: cyrus is starting!!
    Co-authored-by: Marc Bouvier <marc-bouvier@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+
 mkdir -p /etc/pki/cyrus-imapd/
 if [ ! -f /etc/pki/cyrus-imapd/cyrus-imapd.pem ]; then
   echo "--- Generating Cyrus Certificates (in /etc/pki/cyrus-imapd)"
@@ -20,9 +20,12 @@ rm -f /var/lib/cyrus/jwt/jmap.pem
   echo "-----END HMAC KEY-----"
 } > /var/lib/cyrus/jwt/jmap.pem
 
+chown cyrus:mail /var/lib/cyrus/jwt/jmap.pem
 chmod 600 /var/lib/cyrus/jwt/jmap.pem
 
 mkdir -p /run/cyrus/socket && chown -R cyrus:mail /run/
+rsyslogd
+service saslauthd start
 su cyrus -g mail -c "/usr/local/libexec/master -C /etc/imapd.conf -M /etc/cyrus.conf"
 
 echo "Aborted"
